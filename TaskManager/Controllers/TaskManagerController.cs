55c0382d5e0dc8186b198f227843c61abbe70f9c
Braden Jonsson <braden337@gmail.com>
2021-05-21 12:51:22 -0500

    Add TaskManagerController + Membership class

---
+++ TaskManager/Controllers/TaskManagerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    public class TaskManagerController : Controller
+    {
+        public ApplicationDbContext db = new ApplicationDbContext();
+        private UserManager<ApplicationUser> userManager;
+
+        public TaskManagerController()
+        {
+            userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+        }
+
+        public ApplicationUser CurrentUser()
+        {
+            return userManager.FindById(User.Identity.GetUserId());
+        }
+    }
+}
---
+++ TaskManager/Models/Membership.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Models
+{
+    public class Membership
+    {
+        private static ApplicationDbContext db = new ApplicationDbContext();
+
+        private static RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(
+            new RoleStore<IdentityRole>(db)
+        );
+
+        private static UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(
+            new UserStore<ApplicationUser>(db)
+        );
+
+        public static IEnumerable<string> GetAllRoles()
+        {
+            return roleManager.Roles.Select(r => r.Name).ToList();
+        }
+
+        //Check if user is in a role
+        public static bool UserInRole(string userId, string role)
+        {
+            return userManager.IsInRole(userId, role);
+        }
+
+        // Add User To Role
+        public static bool AddUserToRole(string userId, string role)
+        {
+            if (UserInRole(userId, role))
+                return false;
+
+            userManager.AddToRole(userId, role);
+            return true;
+        }
+
+        // Remove User From Role
+        public static bool RemoveUserFromRole(string userId, string role)
+        {
+            if (!UserInRole(userId, role))
+                return false;
+
+            userManager.RemoveFromRole(userId, role);
+            return true;
+        }
+
+        // AddRole
+        public static void AddRole(string role)
+        {
+            if (!roleManager.RoleExists(role))
+                roleManager.Create(new IdentityRole { Name = role });
+        }
+
+        // RemoveRole
+        public static void RemoveRole(string role)
+        {
+            if (roleManager.RoleExists(role))
+                roleManager.Delete(roleManager.FindByName(role));
+        }
+
+        // Get all roles of user
+        public static IEnumerable<string> GetAllRolesOfUser(string userId)
+        {
+            return userManager.GetRoles(userId);
+        }
+    }
+}
