44f9c562fa5cc8fac2da4fa345604b864b8e6d76
Braden Jonsson <braden337@gmail.com>
2021-05-22 13:52:09 -0500

    Use TempData for errors when unable to validate

---
+++ TaskManager/Migrations/202105221837322_MakeTaskNameRequired.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MakeTaskNameRequired : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.ProjectTasks", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.ProjectTasks", "Name", c => c.String());
+        }
+    }
+}
--- TaskManager/Models/ProjectTask.cs
+++ TaskManager/Models/ProjectTask.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -15,6 +16,7 @@ namespace TaskManager.Models
 		public int ID { get; set; }
 		public int ProjectID { get; set; }
 		public int CompletionPercentage { get; set; }
+		[Required]
 		public string Name { get; set; }
 
 		public virtual Project Project { get; set; }
