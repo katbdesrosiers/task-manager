0cee8c7dd1546e5edc38616024b540fc2a70b41d
Katherine Desrosiers <katbdesrosiers@gmail.com>
2021-05-28 09:22:53 -0500

    add notification model

---
+++ TaskManager/Models/Notification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Models
+{
+    public class Notification
+    {
+        public int ScheduledItemID { get; set; }
+        public int UserID { get; set; }
+        public bool Read { get; set; }
+        public string Content { get; set; }
+    }
+}
