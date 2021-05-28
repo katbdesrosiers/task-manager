33f80c672ce91769d10b21e19c3d95bed3a67477
Braden Jonsson <braden337@gmail.com>
2021-05-28 10:45:16 -0500

    Refactor Helpers

---
+++ TaskManager/Models/Helper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskManager.Models
+{
+    public abstract class Helper
+    {
+        protected ApplicationDbContext db { get; set; }
+
+        public static TaskHelper Task (ApplicationDbContext context)
+        {
+            var helper = new TaskHelper();
+            
+            helper.db = context;
+
+            return helper;
+        }
+
+        public static ProjectHelper Project(ApplicationDbContext context)
+        {
+            var helper = new ProjectHelper();
+
+            helper.db = context;
+
+            return helper;
+        }
+    }
+}
