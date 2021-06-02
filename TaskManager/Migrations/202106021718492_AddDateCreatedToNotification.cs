bc70b0454d2986ac1044b31f50999c2511c9529c
Katherine Desrosiers <katbdesrosiers@gmail.com>
2021-06-02 12:19:34 -0500

    add migration for date created

---
+++ TaskManager/Migrations/202106021718492_AddDateCreatedToNotification.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDateCreatedToNotification : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Notifications", "DateCreated", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Notifications", "DateCreated");
+        }
+    }
+}
